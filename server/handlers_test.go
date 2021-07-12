70a121d43026b509d29d5eee60e5b8d784a306b0
Brendan Erwin <brendanjerwin@gmail.com>
2021-07-12 23:27:13 +0000

    ginkgo works

---
+++ server/handlers_test.go
@@ -0,0 +1,14 @@
+package server_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Handlers", func() {
+	Describe("frontmatter API handler", func() {
+		It("should work", func() {
+			Expect(true).To(Equal(true))
+		})
+	})
+})
