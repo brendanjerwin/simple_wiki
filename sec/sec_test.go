89bf7915ac8ac693733617fa1b64210b5230b0f4
Brendan Erwin <brendanjerwin@gmail.com>
2023-11-24 22:37:52 -0500

    tidy ignore and add some test coverage

--- sec/sec_test.go
+++ sec/sec_test.go
@@ -2,14 +2,41 @@ package sec
 
 import "testing"
 
-func TestHashing(t *testing.T) {
-	p := HashPassword("1234")
-	err := CheckPasswordHash("1234", p)
+func TestHashAndCheckPassword(t *testing.T) {
+	password := "mySecurePassword"
+
+	// Hash the password
+	hashedPassword := HashPassword(password)
+
+	// Check the hashed password
+	err := CheckPasswordHash(password, hashedPassword)
+
 	if err != nil {
-		t.Errorf("Should be correct password")
+		t.Errorf("Failed to verify the password: %v", err)
 	}
-	err = CheckPasswordHash("1234lkjklj", p)
+}
+
+func TestHashAndCheckPasswordEdgeCases(t *testing.T) {
+	// Test with incorrect password
+	password := "mySecurePassword"
+	wrongPassword := "wrongPassword"
+	hashedPassword := HashPassword(password)
+	err := CheckPasswordHash(wrongPassword, hashedPassword)
 	if err == nil {
-		t.Errorf("Should NOT be correct password")
+		t.Errorf("Expected an error when checking the wrong password, but didn't get one")
+	}
+
+	// Test with blank password
+	blankPassword := ""
+	hashedBlankPassword := HashPassword(blankPassword)
+	err = CheckPasswordHash(blankPassword, hashedBlankPassword)
+	if err != nil {
+		t.Errorf("Failed to verify the blank password: %v", err)
+	}
+
+	// Test with blank hashed password
+	err = CheckPasswordHash(password, "")
+	if err == nil {
+		t.Errorf("Expected an error when checking with a blank hashed password, but didn't get one")
 	}
 }
