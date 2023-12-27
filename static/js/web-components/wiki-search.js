3eab87ec0d1e40a05300b6196cf76871ea088923
Brendan Erwin <brendanjerwin@gmail.com>
2023-12-27 10:38:28 -0500

    Basic wireup to the API done.

--- static/js/web-components/wiki-search.js
+++ static/js/web-components/wiki-search.js
@@ -42,19 +42,47 @@ export class WikiSearch extends LitElement {
     `;
 
     static properties = {
-        search_endpoint: { type: String },
+        searchEndpoint: { type: String, attribute: 'search-endpoint' },
+        resultArrayPath: { type: String, attribute: 'result-array-path' },
     };
 
     constructor() {
         super();
+        this.resultArrayPath = "results";
+    }
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+        const form = e.target;
+        const search_term = form.q.value;
+        const url = `${this.searchEndpoint}?q=${search_term}`;
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                if (this.resultArrayPath) {
+                    data = this.getNestedProperty(data, this.resultArrayPath);
+                }
+                const event = new CustomEvent('search-results', {
+                    detail: data,
+                });
+                this.dispatchEvent(event);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    getNestedProperty(obj, path) {
+        return path.split('.').reduce((o, p) => (o && o[p]) ? o[p] : null, obj);
     }
 
     render() {
         return html`
     <div id="container">
-        <form action="/search" method="get">
+        <form @submit="${this.handleFormSubmit}">
             <input type="search" name="q" placeholder="Search..." required>
-            <button type="submit">Search</button>
+            <button type="submit"><i class="fa fa-search"></i></button>
         </form>
     </div>
         `;
