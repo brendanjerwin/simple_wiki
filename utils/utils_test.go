02bff23cd55f82b9ac008f33b94bd3540ac7c704
Your Name <you@example.com>
2025-06-24 20:45:55 -0400

    refactor: Convert utils_test.go to Ginkgo/Gomega tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,239 +1,226 @@
-package utils
+package utils_test
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/brendanjerwin/simple_wiki/common"
+	"github.com/brendanjerwin/simple_wiki/index/frontmatter"
 	"github.com/brendanjerwin/simple_wiki/templating"
+	. "github.com/brendanjerwin/simple_wiki/utils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-type MockReadFrontMatter struct {
-	Frontmatter map[string]interface{}
-	Markdown    string
+func TestUtils(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Utils Suite")
 }
 
-func (m *MockReadFrontMatter) ReadFrontMatter(identifier string) (string, map[string]interface{}, error) {
+type MockReadFrontMatter struct {
+	Frontmatter common.FrontMatter
+	Markdown    common.Markdown
+}
+
+func (m *MockReadFrontMatter) ReadFrontMatter(identifier common.PageIdentifier) (common.PageIdentifier, common.FrontMatter, error) {
 	return identifier, m.Frontmatter, nil
 }
 
-func (m *MockReadFrontMatter) ReadMarkdown(identifier string) (string, string, error) {
+func (m *MockReadFrontMatter) ReadMarkdown(identifier common.PageIdentifier) (common.PageIdentifier, common.Markdown, error) {
 	return identifier, m.Markdown, nil
 }
 
 // Mocks index.IQueryFrontmatterIndex
 type MockQueryFrontmatterIndex struct {
-	Results map[string][]string
+	Results        map[string][]common.PageIdentifier
+	GetValueResult frontmatter.Value
 }
 
-func (m *MockQueryFrontmatterIndex) QueryExactMatch(keyPath string, value string) []string {
-	return m.Results[keyPath]
+func (m *MockQueryFrontmatterIndex) QueryExactMatch(keyPath frontmatter.DottedKeyPath, value frontmatter.Value) []common.PageIdentifier {
+	return m.Results[string(keyPath)]
 }
 
-func (m *MockQueryFrontmatterIndex) QueryKeyExistence(keyPath string) []string {
-	return m.Results[keyPath]
+func (m *MockQueryFrontmatterIndex) QueryKeyExistence(keyPath frontmatter.DottedKeyPath) []common.PageIdentifier {
+	return m.Results[string(keyPath)]
 }
 
-func (m *MockQueryFrontmatterIndex) QueryPrefixMatch(keyPath string, valuePrefix string) []string {
-	return m.Results[keyPath]
+func (m *MockQueryFrontmatterIndex) QueryPrefixMatch(keyPath frontmatter.DottedKeyPath, valuePrefix string) []common.PageIdentifier {
+	return m.Results[string(keyPath)]
 }
 
-func (m *MockQueryFrontmatterIndex) GetValue(identifier string, keyPath string) string {
-	return ""
+func (m *MockQueryFrontmatterIndex) GetValue(identifier common.PageIdentifier, keyPath frontmatter.DottedKeyPath) frontmatter.Value {
+	return m.GetValueResult
 }
 
-func BenchmarkAlliterativeAnimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		RandomAlliterateCombo()
-	}
-}
+var _ = Describe("Utils", func() {
+	Describe("ReverseSlice", func() {
+		Describe("ReverseSliceInt64", func() {
+			It("should reverse a slice of int64", func() {
+				slice := []int64{1, 2, 3, 4, 5}
+				reversed := ReverseSliceInt64(slice)
+				Expect(reversed).To(Equal([]int64{5, 4, 3, 2, 1}))
+			})
+		})
 
-func TestReverseList(t *testing.T) {
-	s := []int64{1, 10, 2, 20}
-	if ReverseSliceInt64(s)[0] != 20 {
-		t.Errorf("Could not reverse: %v", s)
-	}
-	s2 := []string{"a", "b", "d", "c"}
-	if ReverseSliceString(s2)[0] != "c" {
-		t.Errorf("Could not reverse: %v", s2)
-	}
-}
+		Describe("ReverseSliceString", func() {
+			It("should reverse a slice of strings", func() {
+				slice := []string{"apple", "banana", "cherry"}
+				reversed := ReverseSliceString(slice)
+				Expect(reversed).To(Equal([]string{"cherry", "banana", "apple"}))
+			})
+		})
 
-func TestMarkdownToHtmlWithFrontmatter(t *testing.T) {
-	markdown := `
+		Describe("ReverseSliceInt", func() {
+			It("should reverse a slice of int", func() {
+				slice := []int{1, 2, 3, 4, 5}
+				reversed := ReverseSliceInt(slice)
+				Expect(reversed).To(Equal([]int{5, 4, 3, 2, 1}))
+			})
+		})
+	})
+
+	Describe("MarkdownToHtmlAndJsonFrontmatter", func() {
+		var (
+			markdown string
+			html     []byte
+		)
+
+		BeforeEach(func() {
+			markdown = `
 ---
 sample: "value"
 ---
 
 # Hello
 	`
-
-	html, _, _ := MarkdownToHtmlAndJsonFrontmatter(markdown, true, &MockReadFrontMatter{}, &GoldmarkRenderer{}, &MockQueryFrontmatterIndex{})
-
-	if strings.Contains(string(html), "sample:") {
-		t.Errorf("Did not remove frontmatter.")
-	}
-
-	if !strings.Contains(string(html), ">Hello</h1") {
-		t.Errorf("Did not include HTML")
-	}
-}
-
-func TestExecuteTemplate(t *testing.T) {
-
-	frontmatter := common.FrontMatter{"identifier": "1234"}
-
-	templateHtml := `
-{{ .Identifier }}
-	`
-
-	rendered, err := templating.ExecuteTemplate(templateHtml, frontmatter, &MockReadFrontMatter{}, &MockQueryFrontmatterIndex{})
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !strings.Contains(string(rendered), "1234") {
-		t.Error("Did not render identifer into output")
-	}
-}
-
-func TestExecuteTemplateUnstructured(t *testing.T) {
-
-	frontmatter := common.FrontMatter{"identifier": "1234", "foobar": "baz"}
-
-	templateHtml := `
-{{ index .Map "foobar" }}
-	`
-
-	rendered, err := templating.ExecuteTemplate(templateHtml, frontmatter, &MockReadFrontMatter{}, &MockQueryFrontmatterIndex{})
-
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !strings.Contains(string(rendered), "baz") {
-		t.Error("Did not render data into output")
-	}
-}
-func TestStripFrontmatter(t *testing.T) {
-	testCases := []struct {
-		name     string
-		input    string
-		expected string
-	}{
-		{
-			name:     "Test with frontmatter",
-			input:    "---\ntitle: Test\n---\nThis is a test",
-			expected: "This is a test",
-		},
-		{
-			name:     "Test without frontmatter",
-			input:    "This is a test",
-			expected: "This is a test",
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			result := StripFrontmatter(tc.input)
-			if result != tc.expected {
-				t.Errorf("expected: %s, got: %s", tc.expected, result)
-			}
+			var err error
+			html, _, err = MarkdownToHtmlAndJsonFrontmatter(markdown, true, &MockReadFrontMatter{}, &GoldmarkRenderer{}, &MockQueryFrontmatterIndex{})
+			Expect(err).NotTo(HaveOccurred())
 		})
-	}
-}
 
-func TestRandomAlliterateCombo(t *testing.T) {
-	combo := RandomAlliterateCombo()
-	if len(combo) == 0 {
-		t.Errorf("Expected a non-empty string, got an empty string")
-	}
-}
+		It("should remove frontmatter from the output", func() {
+			Expect(string(html)).NotTo(ContainSubstring("sample:"))
+		})
 
-func TestStringInSlice(t *testing.T) {
-	strings := []string{"apple", "banana", "cherry"}
-	if !StringInSlice("banana", strings) {
-		t.Errorf("Expected 'banana' to be in the slice")
-	}
-	if StringInSlice("orange", strings) {
-		t.Errorf("Did not expect 'orange' to be in the slice")
-	}
-}
+		It("should render the markdown to HTML", func() {
+			Expect(string(html)).To(ContainSubstring(">Hello</h1"))
+		})
+	})
 
-func TestContentTypeFromName(t *testing.T) {
-	if ContentTypeFromName("file.md") != "text/markdown; charset=utf-8" {
-		t.Errorf("Expected 'text/markdown', got '%s'", ContentTypeFromName("file.md"))
-	}
-	if ContentTypeFromName("image.heic") != "image/heic" {
-		t.Errorf("Expected 'image/heic', got '%s'", ContentTypeFromName("image.heic"))
-	}
-}
+	Describe("templating.ExecuteTemplate", func() {
+		var (
+			theFrontmatter common.FrontMatter
+			rendered       []byte
+			err            error
+		)
+		var site common.PageReader = &MockReadFrontMatter{}
+		var query frontmatter.IQueryFrontmatterIndex = &MockQueryFrontmatterIndex{}
 
-func TestRandomStringOfLength(t *testing.T) {
-	str, err := RandomStringOfLength(10)
-	if err != nil {
-		t.Errorf("Expected no error, got '%s'", err)
-	}
-	if len(str) != 10 {
-		t.Errorf("Expected length 10, got '%d'", len(str))
-	}
-}
+		Describe("When using a simple template", func() {
+			BeforeEach(func() {
+				theFrontmatter = common.FrontMatter{"identifier": "1234"}
+				templateHtml := `{{ .Identifier }}`
+				rendered, err = templating.ExecuteTemplate(templateHtml, theFrontmatter, site, query)
+			})
+			It("should not return an error", func() {
+				Expect(err).NotTo(HaveOccurred())
+			})
+			It("should render identifier into output", func() {
+				Expect(string(rendered)).To(ContainSubstring("1234"))
+			})
+		})
 
-func TestExists(t *testing.T) {
-	if !Exists("./utils.go") {
-		t.Errorf("Expected './utils.go' to exist")
-	}
-	if Exists("./nonexistent_file.go") {
-		t.Errorf("Did not expect './nonexistent_file.go' to exist")
-	}
-}
+		Describe("When using an unstructured map", func() {
+			BeforeEach(func() {
+				theFrontmatter = common.FrontMatter{"identifier": "1234", "foobar": "baz"}
+				templateHTML := `{{ index .Map "foobar" }}`
+				rendered, err = templating.ExecuteTemplate(templateHTML, theFrontmatter, site, query)
+			})
+			It("should not return an error", func() {
+				Expect(err).NotTo(HaveOccurred())
+			})
+			It("should render unstructured data into output", func() {
+				Expect(string(rendered)).To(ContainSubstring("baz"))
+			})
+		})
+	})
 
-func TestEncodeToBase32(t *testing.T) {
-	if EncodeToBase32("hello") != "NBSWY3DP" {
-		t.Error("Expected 'NBSWY3DP", EncodeToBase32("hello"))
-	}
-}
+	Describe("StripFrontmatter", func() {
+		DescribeTable("when stripping frontmatter",
+			func(input string, expected string) {
+				Expect(StripFrontmatter(input)).To(Equal(expected))
+			},
+			Entry("with frontmatter", "---\ntitle: Test\n---\nThis is a test", "This is a test"),
+			Entry("without frontmatter", "This is a test", "This is a test"),
+		)
+	})
 
-func TestDecodeFromBase32(t *testing.T) {
-	str, err := DecodeFromBase32("NBSWY3DP")
-	if err != nil {
-		t.Errorf("Expected no error, got '%s'", err)
-	}
-	if str != "hello" {
-		t.Errorf("Expected 'hello', got '%s'", str)
-	}
-}
+	Describe("RandomAlliterateCombo", func() {
+		It("should return a non-empty string", func() {
+			combo := RandomAlliterateCombo()
+			Expect(combo).NotTo(BeEmpty())
+		})
+	})
 
-func TestReverseSliceInt64(t *testing.T) {
-	slice := []int64{1, 2, 3, 4, 5}
-	reversed := ReverseSliceInt64(slice)
-	expected := []int64{5, 4, 3, 2, 1}
-	for i, v := range reversed {
-		if v != expected[i] {
-			t.Errorf("Expected '%d', got '%d'", expected[i], v)
-		}
-	}
-}
+	Describe("StringInSlice", func() {
+		var sl []string
+		BeforeEach(func() {
+			sl = []string{"apple", "banana", "cherry"}
+		})
+		Describe("When the string is in the slice", func() {
+			It("should return true", func() {
+				Expect(StringInSlice("banana", sl)).To(BeTrue())
+			})
+		})
+		Describe("When the string is not in the slice", func() {
+			It("should return false", func() {
+				Expect(StringInSlice("orange", sl)).To(BeFalse())
+			})
+		})
+	})
 
-func TestReverseSliceString(t *testing.T) {
-	slice := []string{"apple", "banana", "cherry"}
-	reversed := ReverseSliceString(slice)
-	expected := []string{"cherry", "banana", "apple"}
-	for i, v := range reversed {
-		if v != expected[i] {
-			t.Errorf("Expected '%s', got '%s'", expected[i], v)
-		}
-	}
-}
+	Describe("ContentTypeFromName", func() {
+		DescribeTable("when given a filename",
+			func(filename, expectedContentType string) {
+				Expect(ContentTypeFromName(filename)).To(Equal(expectedContentType))
+			},
+			Entry("for a markdown file", "file.md", "text/markdown; charset=utf-8"),
+			Entry("for a heic file", "image.heic", "image/heic"),
+		)
+	})
 
-func TestReverseSliceInt(t *testing.T) {
-	slice := []int{1, 2, 3, 4, 5}
-	reversed := ReverseSliceInt(slice)
-	expected := []int{5, 4, 3, 2, 1}
-	for i, v := range reversed {
-		if v != expected[i] {
-			t.Errorf("Expected '%d', got '%d'", expected[i], v)
-		}
-	}
-}
+	Describe("RandomStringOfLength", func() {
+		It("should return a string of the specified length", func() {
+			str, err := RandomStringOfLength(10)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(str)).To(Equal(10))
+		})
+	})
+
+	Describe("Exists", func() {
+		Describe("When the file exists", func() {
+			It("should return true", func() {
+				Expect(Exists("./utils.go")).To(BeTrue(), "Expected file utils.go to exist in current directory for test.")
+			})
+		})
+		Describe("When the file does not exist", func() {
+			It("should return false", func() {
+				Expect(Exists("./nonexistent_file.go")).To(BeFalse())
+			})
+		})
+	})
+
+	Describe("Base32 encoding/decoding", func() {
+		Describe("EncodeToBase32", func() {
+			It("should encode a string to base32", func() {
+				Expect(EncodeToBase32("hello")).To(Equal("NBSWY3DP"))
+			})
+		})
+		Describe("DecodeFromBase32", func() {
+			It("should decode a base32 string", func() {
+				str, err := DecodeFromBase32("NBSWY3DP")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(str).To(Equal("hello"))
+			})
+		})
+	})
+})
