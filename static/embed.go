f7f462673c8453a570cf3a0ee011e09ea52bfede
Brendan Erwin <3039+brendanjerwin@users.noreply.github.com>
2025-07-16 22:16:28 -0400

    This is why it didn't build.

--- static/embed.go
+++ static/embed.go
@@ -8,6 +8,6 @@ import (
 //go:embed templates/index.tmpl
 var IndexTemplate string // IndexTemplate is the main HTML template.
 
-//go:generate bash -c "cd js && bun run build"
+//go:generate bash -c "cd js && bun install && bun run build"
 //go:embed **
 var StaticContent embed.FS // StaticContent contains all embedded static files.
