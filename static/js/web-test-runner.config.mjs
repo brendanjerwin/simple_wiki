fed1998f3d868d0ac37fe242e7bc75ad6033805a
Brendan Erwin <3039+brendanjerwin@users.noreply.github.com>
2025-07-18 12:05:22 -0400

    fix(js-tests): Make browser log filter more specific
    Updates the `filterBrowserLogs` function to use an exact match for the
    Lit dev mode warning.
    
    This makes the filter more brittle, as requested, to avoid
    accidentally suppressing other console messages.

--- static/js/web-test-runner.config.mjs
+++ static/js/web-test-runner.config.mjs
@@ -1,5 +1,6 @@
 import { chromeLauncher } from '@web/test-runner-chrome';
 import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { summaryReporter } from '@web/test-runner';
 
 const chromiumPath = process.env.CHROMIUM_BIN;
 console.log('WTR Config - CHROMIUM_BIN:', chromiumPath);
@@ -8,6 +9,7 @@ export default {
   files: './web-components/**/*.test.ts',
   base: '/static/js/',
   nodeResolve: true,
+  reporters: [summaryReporter()],
   browsers: [
     chromeLauncher({
       executablePath: chromiumPath,
@@ -33,4 +35,12 @@ export default {
     },
   },
   testsFinishTimeout: 180000,
+  filterBrowserLogs(log) {
+    // This is the full message that Lit logs to the console.
+    const litDevModeMessage = 'Lit is in dev mode. Not recommended for production! See https://lit.dev/msg/dev-mode for more information.';
+    return !log.args.some(
+      arg =>
+        typeof arg === 'string' && arg === litDevModeMessage,
+    );
+  },
 };
