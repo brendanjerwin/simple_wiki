172b3e5c1c754c4b539ffdb2016ade138b32ea7f
Brendan Erwin <3039+brendanjerwin@users.noreply.github.com>
2025-07-27 11:33:51 -0400

    lint fix

--- static/js/web-components/frontmatter-editor-dialog.stories.ts
+++ static/js/web-components/frontmatter-editor-dialog.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/web-components';
+import type { Meta, StoryObj } from '@storybook/web-components-vite';
 import { action } from 'storybook/actions';
 import { html } from 'lit';
 import './frontmatter-editor-dialog.js';
