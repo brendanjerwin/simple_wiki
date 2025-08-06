9bd88ebb94860b3edbb2d703a48062de23c15f6e
Brendan Erwin <3039+brendanjerwin@users.noreply.github.com>
2025-08-06 15:17:18 -0400

    Delete migrations/eager/file_shadowing_migration_test.go

--- migrations/eager/file_shadowing_migration_test.go
+++
@@ -1,5 +0,0 @@
-// This file is intentionally empty - tests have been moved to separate files:
-// - file_shadowing_migration_scan_job_test.go  
-// - file_shadowing_migration_job_test.go
-
-package eager
